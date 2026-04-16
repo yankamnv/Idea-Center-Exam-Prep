1182db8f4d9db53a6ce8d18918af800a8dcbedf4
yankamnv <manevayanka86@gmail.com>
2026-04-16 21:05:42 +0300

    Added Nunit projects

---
+++ ExamPrep1/Models/ApiResponseDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ExamPrep1.Models
+{
+    internal class ApiResponseDTO
+    {
+        [JsonPropertyName("msg")]
+
+        public string Msg { get; set; }
+
+        [JsonPropertyName("id")]
+
+        public string Id { get; set; }
+    }
+}
---
+++ ExamPrep1/UnitTest1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using ExamPrep1.Models;
+
+
+
+namespace ExamPrep1
+{
+    [TestFixture]
+    public class Tests
+    {
+        private RestClient client;
+        private static string lastCreatedIdeaId;
+
+
+        private const string BaseUrl = "http://144.91.123.158:82";
+        private const string StaticToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJKd3RTZXJ2aWNlQWNjZXNzVG9rZW4iLCJqdGkiOiIzYzEyZTQ3NS01MjVjLTQwOWMtYjFhZi1kNWRmNzRlNzIwOTgiLCJpYXQiOiIwNC8xNi8yMDI2IDEyOjUwOjM2IiwiVXNlcklkIjoiODVkNTRiYzgtM2IwMy00NDk3LTUzYjYtMDhkZTc2YTJkM2VjIiwiRW1haWwiOiJ5YW5rYXNAbS5jb20iLCJVc2VyTmFtZSI6InlhbmthUyIsImV4cCI6MTc3NjM2NTQzNiwiaXNzIjoiSWRlYUNlbnRlcl9BcHBfU29mdFVuaSIsImF1ZCI6IklkZWFDZW50ZXJfV2ViQVBJX1NvZnRVbmkifQ.EP6liRN2SuJaY3fEBsO7340GZ7EEQ9kqasparjzAQbU";
+
+        private const string LoginEmail = "yankas@m.com";
+        private const string LoginPassword = "123456";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            string jwtToken;
+
+            if(!string.IsNullOrWhiteSpace(StaticToken))
+            {
+                jwtToken = StaticToken;
+            }
+            else
+            {
+                jwtToken = GetJwtToken(LoginEmail, LoginPassword);
+            }
+
+            var options = new RestClientOptions(BaseUrl)
+            {
+                Authenticator = new JwtAuthenticator(jwtToken)
+            };
+
+            this.client = new RestClient(options);
+        }
+
+        private string GetJwtToken(string email, string password)
+        {
+            var tempClient = new RestClient(BaseUrl);
+            var request = new RestRequest("/api/User/Authenticate", Method.Post);
+
+            request.AddJsonBody(new { email, password });
+
+            var response = tempClient.Execute(request);
+
+            if(response.StatusCode == HttpStatusCode.OK)
+            {
+                var content = JsonSerializer.Deserialize<JsonElement>(response.Content);
+                var token = content.GetProperty("token").GetString();
+
+                if(string.IsNullOrWhiteSpace(token))
+                {
+                    throw new InvalidOperationException("Token not found in the response.");
+                }
+                return token;
+            }
+            else
+            {
+                throw new InvalidOperationException("Filed to authenticate.");
+            }
+        }
+
+        [Order(1)]
+        [Test]
+        public void CreateIdea_WithRequiredFields_ShouldReturnSuccess()
+        {
+            var ideaData = new IdeaDTO
+            {
+                Title = "Test Title",
+                Description = "Test Description",
+                Url = ""
+            };
+
+            var request = new RestRequest("/api/Idea/Create", Method.Post);
+            request.AddJsonBody(ideaData);
+
+            var response = this.client.Execute(request);
+
+            var createResponse = JsonSerializer.Deserialize<ApiResponseDTO>(response.Content);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(createResponse.Msg, Is.EqualTo("Successfully created!"));
+
+        }
+
+        [Order(2)]
+        [Test]
+        public void GetAllIdeas_ShouldReturnSuccess()
+        {
+            var request = new RestRequest("/api/Idea/All", Method.Get);
+            var response = this.client.Execute(request);
+
+            var responseItems = JsonSerializer.Deserialize<List<ApiResponseDTO>>(response.Content);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(responseItems, Is.Not.Empty);
+            Assert.That(responseItems, Is.Not.Null);
+
+            lastCreatedIdeaId = responseItems.LastOrDefault().Id;
+        }
+
+        [Order(3)]
+        [Test]
+        public void EditLastIdeaCreated_ShouldReturnSuccess()
+        {
+            var editRequestData = new IdeaDTO
+            {
+                Title = "Edited Title",
+                Description = "Edited Description",
+                Url = ""
+            };
+
+            var request = new RestRequest("/api/Idea/Edit", Method.Put);
+
+            request.AddQueryParameter("ideaId", lastCreatedIdeaId);
+            request.AddJsonBody(editRequestData);
+
+            var response = this.client.Execute(request);
+
+            var editResponse = JsonSerializer.Deserialize<ApiResponseDTO>(response.Content);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(editResponse.Msg, Is.EqualTo("Edited successfully"));
+        }
+
+        [Order(4)]
+        [Test]
+        public void DeleteLastCreatedIdea_ShouldReturnSuccess()
+        {
+            var request = new RestRequest("/api/Idea/Delete", Method.Delete);
+            request.AddQueryParameter("ideaId", lastCreatedIdeaId);
+            var response = this.client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.Content, Is.EqualTo("\"The idea is deleted!\""));
+        }
+
+        [Order(5)]
+        [Test]
+        public void CreateIdeaWithoutRequiredFields_ShouldReturnBadRequest()
+        {
+            var ideaData = new IdeaDTO
+            {
+                Title = "",
+                Description = "Description",
+                Url = ""
+            };
+
+            var request = new RestRequest("/api/Idea/Create", Method.Post);
+            request.AddJsonBody(ideaData);
+
+            var response = this.client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        }
+
+        [Order(6)]
+        [Test]
+        public void EditANonExistingIdea_ShouldReturnBadRequest()
+        {
+            string nonExistingId = "999";
+
+            var editedIdeaData = new IdeaDTO
+            {
+                Title = "Edited title",
+                Description = "Edited Description",
+                Url = ""
+            };
+
+            var request = new RestRequest("/api/Idea/Edit", Method.Put);
+            request.AddQueryParameter("ideaId", nonExistingId);
+            request.AddJsonBody(editedIdeaData);
+
+            var response = this.client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+            Assert.That(response.Content, Is.EqualTo("\"There is no such idea!\""));
+        }
+
+        [Order(7)]
+        [Test]
+        public void DeleteNonExistingIdea_ShouldReturnBadRequest()
+        {
+            string nonExistingId = "333";
+
+            var request = new RestRequest("/api/Idea/Delete", Method.Delete);
+            request.AddQueryParameter("ideaId", nonExistingId);
+            var response = this.client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+            Assert.That(response.Content, Is.EqualTo("\"There is no such idea!\""));
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() 
+        {
+            this.client.Dispose();
+        }
+    }
+}
